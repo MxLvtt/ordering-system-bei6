13aa2c2ae49c9b7410af55089c9ae248cda58842
Marcel Livotto <marcel-livotto@web.de>
2020-04-07 17:11:01 +0200

    Add temp files to help development on order tiles

---
+++ src/UI/Temp/ordertile_test.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+"""
+
+Das ist die Datei, die du dann umsetzen sollst.
+
+"""
+
+# OrderTileTest Klassen-Definition; Erbt von "Frame" (eine Klasse von tkinter)
+# Ein Frame ist einfach eine leere Fläche in die wir andere Elemente einfügen können, z.B. Buttons
+class OrderTileTest(Frame):
+    # Konstruktor der Klasse OrderTileTest
+    def __init__(
+        self, # das Argument muss immer in den Konstruktor, wird aber beim Aufrufen des Konstruktors (siehe 'ordertile_testing_area.py')
+              # nicht gesetzt!
+        parent, # Gibt an, zu welchem übergeordneten tkinter-Element dieses Frame eingefügt werden soll
+        background="white" # Setzt die Hintergrundfarbe des Ordertiles, default Wert ist 'white'
+        # Hier könnten noch weitere Parameter hinzugefügt werden
+    ):
+        # Mit super().__init__(...) rufen wir den Konstruktor von Frame (der Oberklasse) auf;
+        # Dadurch wird im Hintergrund das eigentliche Frame Objekt erzeugt
+        super().__init__(
+            master=parent,
+            background=background
+            # Hier könnten noch weitere Parameter von 'Frame' gesetzt werden
+        )
+
+        # Hier fügen wir ein Label (=Feld mit Text) zu unserem OrderTile hinzu
+        # master=self: Das sagt dem Label, dass es zum OrderTile gehört
+        # .grid(row=0,column=0): Das setzt das Label in das 'Grid' des OrderTiles in Zeile 0 und Spalte 0;
+        #                        Wenn mehere Elemente gesetzt werden, kann man diese so an verschiedenen Stellen platzieren
+        Label(master=self, text="Das ist ein Beispiel Text").grid(row=0, column=0)
+
+        def funktion_wenn_button_gedrueckt():
+            print("Button gedrueckt!")
+        
+        # sticky="W" oder sticky=W : Sagt dem Button, dass er sich links (W = West) anordnen soll.
+        # Mit "E" (= East) wäre der Button am rechten Rand angeordnet
+        Button(master=self, text="Button", command=funktion_wenn_button_gedrueckt).grid(row=1, column=0, sticky="W")
---
+++ src/UI/Temp/ordertile_testing_area.py
@@ -0,0 +1,39 @@
+from tkinter import *
+# Importiere die andere Klasse, damit wir ein OrderTile benutzen können
+from ordertile_test import OrderTileTest
+
+"""
+
+Alles was hier drin passiert, ist nicht Teil deiner Aufgabe.
+Diese Datei dient nur zum testen deiner Fortschritte mit dem OrderTile,
+der Code hier wird dann in der KitchenUI umgesetzt.
+
+"""
+
+# Platzhalter Variablen für die Größe des 'Order Tiles'
+TILE_WIDTH = 400
+TILE_HEIGHT = 600
+
+# Erstellt ein objekt (root) des fensters
+root = Tk()
+# Setzt den Titel des Fensters
+root.wm_title("Test Window For Order-Tile Development")
+# Setzt die Hintergrundfarbe des Fensters (#696969 = Dunkelgrau)
+root.config(background="#696969")
+# Setzt die Größe des Fensters mit Format: <width>x<height>
+root.geometry(f"{TILE_WIDTH}x{TILE_HEIGHT}+0+0")
+# Gibt an, ob die Größe des Fenster geändert werden darf
+# Argumente: x-resizable, y-resizable
+root.resizable(True, True)
+
+# Fügt ein 'OrderTile' zum Fenster hinzu und füllt das ganze Fenster damit (fill='both');
+# 'both' heißt er füllt es in x- und y-Richtung. Ansonsten nur fill='x' oder 'y' angeben;
+# Der 'OrderTile' soll auch die Größe verändern, wenn sich die Größe des Fensters ändert (expand=1);
+ordertile_objekt = OrderTileTest(parent=root, background='green')
+ordertile_objekt.pack(expand=1, fill='both')
+# Alle Funktionen und Argumente kommen von der Klasse "Frame", von der die 'OrderTileTest' Klasse erbt;
+# Deswegen können wir die auch bei 'OrderTileTest' benutzen. Man kann auch einfach sagen:
+# Frame(root, bg='green').pack(expand=1, fill='both')
+
+# Startet die main loop, so dass das Fenster angezeigt bleibt, bis man es schließt
+mainloop()
