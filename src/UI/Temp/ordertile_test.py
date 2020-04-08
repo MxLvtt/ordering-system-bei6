b2296c173f8569855ceb1dbb97c5b6ed1e43a666
Marcel Livotto <marcel-livotto@web.de>
2020-04-08 15:43:26 +0200

    Updated Order Tile Test class

--- src/UI/Temp/ordertile_test.py
+++ src/UI/Temp/ordertile_test.py
@@ -20,16 +20,30 @@ class OrderTileTest(Frame):
         # Mit super().__init__(...) rufen wir den Konstruktor von Frame (der Oberklasse) auf;
         # Dadurch wird im Hintergrund das eigentliche Frame Objekt erzeugt
         super().__init__(
-            master=parent,
-            background=background
+            master=parent
             # Hier könnten noch weitere Parameter von 'Frame' gesetzt werden
         )
+
+        self.timestamp = "15:08:12"
+        self.meals = ["Meal1", "Meal2", "Meal3"];
+
+        self.status_codes = [ "P", "C", "U" ] # Prepared, Canceled, Unprepared
+        self.status_color_codes = [ "green", "red", "gray" ] # Prepared, Canceled, Unprepared
+
+        self.status = 0 # <- Property
+
+        self._change_background(color=self.status_color_codes[self.status])
+
+        self.status_label = Label(text=self.status_codes[self.status]) # <- UI Element zur Property
+
+        self.order_type = ""
 
         # Hier fügen wir ein Label (=Feld mit Text) zu unserem OrderTile hinzu
         # master=self: Das sagt dem Label, dass es zum OrderTile gehört
         # .grid(row=0,column=0): Das setzt das Label in das 'Grid' des OrderTiles in Zeile 0 und Spalte 0;
         #                        Wenn mehere Elemente gesetzt werden, kann man diese so an verschiedenen Stellen platzieren
-        Label(master=self, text="Das ist ein Beispiel Text").grid(row=0, column=0)
+        Label(master=self, text=self.timestamp).grid(row=0, column=0)
+
 
         def funktion_wenn_button_gedrueckt():
             print("Button gedrueckt!")
@@ -37,3 +51,9 @@ class OrderTileTest(Frame):
         # sticky="W" oder sticky=W : Sagt dem Button, dass er sich links (W = West) anordnen soll.
         # Mit "E" (= East) wäre der Button am rechten Rand angeordnet
         Button(master=self, text="Button", command=funktion_wenn_button_gedrueckt).grid(row=1, column=0, sticky="W")
+
+    def _change_background(self, color: str):
+        self.config(background=color)
+
+    def _change_status(self, new_status: int) -> int:
+        pass
