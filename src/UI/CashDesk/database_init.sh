16cab129185f93f583543681ac87323b39a70cd2
Mxrcel <m.livotto@web.de>
2020-09-01 16:06:01 +0200

    Fix and finish database initialization scripts

--- src/UI/CashDesk/database_init.sh
+++ src/UI/CashDesk/database_init.sh
@@ -2,7 +2,13 @@
 
 echo "Starting initialization of database 'ordsys'...";
 
+echo "    Creating database, table and users";
+
 mysql -u root "-pYal#8925xNxx" < "database_init.sql";
+
+echo "    Filling table";
+
+mysql -u root "-pYal#8925xNxx" < "database_fill_table.sql";
 
 echo "Done";
 
--- src/UI/CashDesk/database_init.sql
+++ src/UI/CashDesk/database_init.sql
@@ -1,16 +1,16 @@
 # Create Database 'ordsys'
-CREATE DATABASE IF NOT EXISTS `ordsys2`;
-USE `ordsys2`;
+CREATE DATABASE IF NOT EXISTS `ordsys`;
+USE `ordsys`;
 
 # Create Table 'orders'
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
-	`id` int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`timestamp` int(10) unsigned NOT NULL,
-	`form` int(10) unsigned NOT NULL DEFAULT 0,
-	`state` int(10) unsigned NOT NULL DEFAULT 0,
+	`id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`timestamp` int unsigned NOT NULL,
+	`form` int unsigned NOT NULL DEFAULT 0,
+	`state` int unsigned NOT NULL DEFAULT 0,
 	`active` varchar(1) NOT NULL DEFAULT 'Y',
-	`price` float unsigned NOT NULL DEFAULT 0,
+	`price` float unsigned DEFAULT 0,
 	`meals` text NOT NULL
 );
 
@@ -23,32 +23,15 @@ CREATE TABLE `meals` (
 	`zutaten` varchar(200),
 	`addons` varchar(200),
 	`groessen` varchar(150),
-	`price` float unsigned DEFAULT 0,
+	`price` float unsigned DEFAULT 0
 );
 
 # Create User for read-only access
-CREATE USER IF NOT EXISTS 'ordsys2RD'@'%' IDENTIFIED BY 'Pa$sw0rd';
-GRANT SELECT ON `ordsys2`.* TO 'ordsys2RD'@'%' IDENTIFIED BY 'Pa$sw0rd';
+DROP USER IF EXISTS 'ordsysRD'@'%';
+CREATE USER IF NOT EXISTS 'ordsysRD'@'%' IDENTIFIED BY 'he=h5bY&x#Lb/=$';
+GRANT SELECT ON `ordsys`.* TO 'ordsysRD'@'%' IDENTIFIED BY 'he=h5bY&x#Lb/=$';
 
 # Create User for read-write access
-CREATE USER IF NOT EXISTS 'ordsys2RW'@'localhost' IDENTIFIED BY 'Pa$sw0rd';
-GRANT ALL PRIVILEGES ON `ordsys2`.* TO 'ordsys2RW'@'localhost' IDENTIFIED BY 'Pa$sw0rd';
-
-# Fill 'meals' Table
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Currywurst mit Pommes',		5.5);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Döner Box',					4);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Döner Teller',					7);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger mit Pommes',		6.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger',					4.5);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger mit Bacon',		4.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger Bacon Pommes',	6.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Chickenburger mit Pommes',		6.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Chickenburger',				4.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Whiteburger mit Pommes',		8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Whiteburger',					6);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Veggieburger mit Pommes',		6.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Veggieburger',					4.8);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Pommes Groß',					3);
-INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Pommes Klein',					2);
-
-exit;
+DROP USER IF EXISTS 'ordsysRW'@'localhost';
+CREATE USER IF NOT EXISTS 'ordsysRW'@'localhost' IDENTIFIED BY '#4}Yjen$]nP';
+GRANT ALL PRIVILEGES ON `ordsys`.* TO 'ordsysRW'@'localhost' IDENTIFIED BY '#4}Yjen$]nP';
