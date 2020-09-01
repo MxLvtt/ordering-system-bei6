a660a3d711c218c29455998b62601559a39e3614
Mxrcel <m.livotto@web.de>
2020-09-01 16:07:35 +0200

    FINAL: Finish database initialization scripts

---
+++ src/UI/CashDesk/database_fill_table.sql
@@ -0,0 +1,18 @@
+USE `ordsys2`;
+
+# Fill 'meals' Table
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Currywurst mit Pommes',		5.5);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Döner Box',					4);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Döner Teller',					7);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger mit Pommes',		6.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger',					4.5);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger mit Bacon',		4.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Cheeseburger Bacon Pommes',	6.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Chickenburger mit Pommes',		6.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Chickenburger',				4.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Whiteburger mit Pommes',		8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Whiteburger',					6);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Veggieburger mit Pommes',		6.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen/Burger',	'Veggieburger',					4.8);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Pommes Groß',					3);
+INSERT INTO `meals` (kategorie,name,price) VALUES ('Essen',			'Pommes Klein',					2);
